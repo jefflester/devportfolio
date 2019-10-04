4e225abc5af1737f5515d2cb4ad1f54de8660570
Jeff Lester <jeffrey.d.lester@gmail.com>
2019-10-03 20:59:04 -0400

    Updated comments/file renames.

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,3 @@
-/*!
-    Title: Dev Portfolio Template
-    Version: 1.2.1
-    Last Change: 08/27/2017
-    Author: Ryan Fitzgerald
-    Repo: https://github.com/RyanFitzgerald/devportfolio-template
-    Issues: https://github.com/RyanFitzgerald/devportfolio-template/issues
-
-    Description: This file contains all the scripts associated with the single-page
-    portfolio website.
-*/
 ! function(n) {
     n("html").removeClass("no-js"), n("header a").click(function(e) {
         if (!n(this).hasClass("no-scroll")) {
